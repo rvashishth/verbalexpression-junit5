ed6c302c1d3e9ac1cb2036ee488f7ad86b7c9155
Rahul Vashishth <rahul.vashishth@airtel.com>
2018-08-01 12:26:16 +0530

    first commit mobile number validation

---
+++ src/main/java/com/wolftechnica/verbalexpression/RegExSamples.java
@@ -0,0 +1,18 @@
+package com.wolftechnica.verbalexpression;
+
+import ru.lanwen.verbalregex.VerbalExpression;
+
+public class RegExSamples {
+
+	public boolean validateMobileNumber(String mobilenumber){
+		VerbalExpression testRegex = VerbalExpression.regex()
+				.startOfLine()
+				.then("234")
+				.capture().range("0","9").count(10).endCapture()
+				.endOfLine().build();
+		boolean testExact = testRegex.testExact(mobilenumber);
+		System.out.println(testExact);
+		return testExact;
+	}
+
+}
---
+++ src/test/java/com/wolftechnica/verbalexpression/test/TestRegExSamples.java
@@ -0,0 +1,38 @@
+package com.wolftechnica.verbalexpression.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import com.wolftechnica.verbalexpression.RegExSamples;
+
+@DisplayName("VariableExpression Test Cases")
+public class TestRegExSamples {
+	
+
+	private static RegExSamples regExSamples;
+	
+	@BeforeAll
+	static void init(){
+		regExSamples = new RegExSamples();
+	}
+
+	
+	@DisplayName("Test if 2348907651230 is valid nigeria mobile number")
+	@Test
+	public void testMobileNumber(){
+		String mobileNumber = "2348907651230";
+		boolean validateMobileNumber = regExSamples.validateMobileNumber(mobileNumber);
+		Assertions.assertTrue(validateMobileNumber);
+	}
+	
+	@DisplayName("Test if 2358907651230 is invalid nigeria mobile number")
+	@Test
+	public void testMobileNumber2(){
+		String mobileNumber = "2358907651230";
+		boolean validateMobileNumber = regExSamples.validateMobileNumber(mobileNumber);
+		Assertions.assertFalse(validateMobileNumber);
+	}
+}
